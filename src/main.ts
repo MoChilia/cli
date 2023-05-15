2e207f997be2219c28ba87155b4c399d301ea888
MoChilia <shiyingchen@microsoft.com>
2023-05-15 17:27:12 +0800

    docker run

--- src/main.ts
+++ src/main.ts
@@ -148,7 +148,7 @@ const executeDockerCommand = async (dockerCommand: string, continueOnError: bool
     };
     var exitCode;
     try {
-        exitCode = await exec.exec(`sudo docker ${dockerCommand}`, [], execOptions);
+        exitCode = await exec.exec(`docker ${dockerCommand}`, [], execOptions);
     } catch (error) {
         if (!continueOnError) {
             throw error;
