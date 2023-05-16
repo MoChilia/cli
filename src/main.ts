b17deff451a61ad9d97676a84dc571bc1a5f7269
MoChilia <shiyingchen@microsoft.com>
2023-05-16 11:50:31 +0800

    deprecate home

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,7 @@ export async function main(){
         let command: string = `run --privileged=true --workdir ${process.env.GITHUB_WORKSPACE}`;
         command += ` -v ${process.env.GITHUB_WORKSPACE}:${process.env.GITHUB_WORKSPACE} `;
         // command += ` -v ${process.env.HOME}/.azure:/root/.azure `;
-        command += ` --mount type=bind,source="${process.env.HOME}/.azure",target=/root/.azure `;
+        command += ` --mount type=bind,source="/runner/.azure",target=/root/.azure `;
         command += ` -v ${TEMP_DIRECTORY}:${TEMP_DIRECTORY} `;
         // command += ` -v ${process.env.HOME}/test:/runner/_work/test `; // TODO: remove this line after testing
         command += ` ${environmentVariables} `;
